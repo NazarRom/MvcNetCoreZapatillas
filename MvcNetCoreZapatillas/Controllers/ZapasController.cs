2c0ef6e0570d92a1a5bd13872e75bec0072f1baa
NazarRom <nazarrom321@gmail.com>
2023-03-16 14:28:35 +0100

    commit

--- MvcNetCoreZapatillas/Controllers/ZapasController.cs
+++ MvcNetCoreZapatillas/Controllers/ZapasController.cs
@@ -28,29 +28,29 @@ namespace MvcNetCoreZapatillas.Controllers
         public IActionResult _ImagenZapatillas(int idproducto, int? posicion)
         {
 
-            if (posicion == null)
-            {
-                posicion = 1;
-            }
-            int numregistros = this.repo.GetNumeroDeImagenes(idproducto);
-            //ESTAMOS EN LA POSICION 1, QUE TENEMOS QUE DEVOLVER A LA VISTA?
-            int siguiente = posicion.Value + 1;
-            if (siguiente > numregistros)
-            {
-                //EFECTO OPTICO
-                siguiente = numregistros;
-            }
-            int anterior = posicion.Value - 1;
-            if (anterior < 1)
-            {
-                anterior = 1;
-            }
-            VistaDepartamento vistaDepartamento =
-                await this.repo.GetVistaDepartamentoAsync(posicion.Value);
-            ViewData["ULTIMO"] = numregistros;
-            ViewData["SIGUIENTE"] = siguiente;
-            ViewData["ANTERIOR"] = anterior;
-            return View(vistaDepartamento);
+            //if (posicion == null)
+            //{
+            //    posicion = 1;
+            //}
+            //int numregistros = this.repo.GetNumeroDeImagenes(idproducto);
+            ////ESTAMOS EN LA POSICION 1, QUE TENEMOS QUE DEVOLVER A LA VISTA?
+            //int siguiente = posicion.Value + 1;
+            //if (siguiente > numregistros)
+            //{
+            //    //EFECTO OPTICO
+            //    siguiente = numregistros;
+            //}
+            //int anterior = posicion.Value - 1;
+            //if (anterior < 1)
+            //{
+            //    anterior = 1;
+            //}
+            //VistaDepartamento vistaDepartamento =
+            //    await this.repo.GetVistaDepartamentoAsync(posicion.Value);
+            //ViewData["ULTIMO"] = numregistros;
+            //ViewData["SIGUIENTE"] = siguiente;
+            //ViewData["ANTERIOR"] = anterior;
+            //return View(vistaDepartamento);
 
 
             List<ImagenZapatilla> imagenZapatillas = this.repo.FindImagenZapatilla(idproducto);
