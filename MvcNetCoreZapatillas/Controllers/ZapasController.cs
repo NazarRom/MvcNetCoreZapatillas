a9bb6315cb3ff556e3bb23fe9ec4203ab1532620
NazarRom <nazarrom321@gmail.com>
2023-03-16 14:24:02 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapasController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers
+{
+    public class ZapasController : Controller
+    {
+        private RepositoryZapatillas repo;
+
+        public ZapasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Zapatilla> zapatillas = this.repo.GetZapatillas();
+            return View(zapatillas);
+        }
+
+        public IActionResult Details(int idproducto)
+        {
+
+            Zapatilla zapatilla = this.repo.FindZapatilla(idproducto);
+            return View(zapatilla);
+        }
+
+        public IActionResult _ImagenZapatillas(int idproducto, int? posicion)
+        {
+
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            int numregistros = this.repo.GetNumeroDeImagenes(idproducto);
+            //ESTAMOS EN LA POSICION 1, QUE TENEMOS QUE DEVOLVER A LA VISTA?
+            int siguiente = posicion.Value + 1;
+            if (siguiente > numregistros)
+            {
+                //EFECTO OPTICO
+                siguiente = numregistros;
+            }
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            VistaDepartamento vistaDepartamento =
+                await this.repo.GetVistaDepartamentoAsync(posicion.Value);
+            ViewData["ULTIMO"] = numregistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            return View(vistaDepartamento);
+
+
+            List<ImagenZapatilla> imagenZapatillas = this.repo.FindImagenZapatilla(idproducto);
+            return PartialView("_ImagenZapatillas", imagenZapatillas);
+        }
+
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region SQL SERVER
+//VUESTRO PROCEDIMIENTO DE PAGINACION DE IMAGENES DE ZAPATILLAS
+
+//create procedure sp_imagen_zapatillas
+//(@idproducto int)
+//AS
+// select idimagen, idproducto, imagen from
+//    (SELECT CAST(
+//    ROW_NUMBER() OVER(ORDER BY idproducto) AS INT) AS POSICION,
+//    ISNULL(IDIMAGEN, 0) AS IDIMAGEN, IDPRODUCTO, IMAGEN 
+//    FROM IMAGENESZAPASPRACTICA
+//	where idproducto = @idproducto) as query
+
+//    where query.POSICION >= 1 and 1 < (1+2)
+//GO
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Zapatilla> GetZapatillas()
+        {
+            return this.context.Zapatillas.ToList();
+        }
+
+        public Zapatilla FindZapatilla(int idproducto)
+        {
+            return this.context.Zapatillas.FirstOrDefault(x => x.IdProducto == idproducto);
+        }
+
+        public List<ImagenZapatilla> FindImagenZapatilla(int idproducto)
+        {
+            string sql = "sp_imagen_zapatillas @idproducto";
+            SqlParameter pamid = new SqlParameter("@idproducto", idproducto);
+            var consulta = this.context.ImagenesZapatillas.FromSqlRaw(sql, pamid);
+            List<ImagenZapatilla> imagens = consulta.ToList();
+            return imagens;
+        }
+
+        public int GetNumeroDeImagenes(int idproducto)
+        {
+            var consulta = (from data in this.context.ImagenesZapatillas
+                           where data.IdProducto == idproducto
+                           select data).Count();
+            return consulta;
+        }
+
+    }
+}
